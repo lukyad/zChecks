e8da1aff2ce5d802c9b305b1dd2ef7a7aa5a4353
lukyad <dmitry.al.lukyanov@gmail.com>
2019-08-20 22:15:10 +0300

    Recreate Sample

--- Sample/Properties/AssemblyInfo.cs
+++ Sample/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("56be58c9-afc4-4064-b4aa-b8794ebbe23c")]
+[assembly: Guid("22892f7e-5faa-44cb-9280-8ccba71448cd")]
 
 // Version information for an assembly consists of the following four values:
 //
